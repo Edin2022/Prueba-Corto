39f74d88e9963e246416ab5b74d2518249a41262
¨Janeth <jdeleon780@gmail.com>
2024-02-24 14:07:17 -0600

    Ejercicio 2

--- main.cpp
+++ main.cpp
@@ -13,5 +13,25 @@ int main() {
         cout << "año de nacimiento valido " << endl;
     else
         cout << "año de nacimiento invalido " << endl;
+
+    const int cantidadNumeros = 5; // Definimos que se ingresarán 5 números
+
+    double suma = 0.0;
+    int contador = 0;
+
+    // Solicitar al usuario que ingrese los 5 números
+    while (contador < cantidadNumeros) {
+        double numero;
+        cout << "Ingrese el numero " << contador + 1 << ": ";
+        cin >> numero;
+        suma += numero; // Sumar el número ingresado a la suma total
+        contador++; // Incrementar el contador
+    }
+
+    // Calcular la media dividiendo la suma total entre la cantidad de números
+    double media = suma / cantidadNumeros;
+
+    // Mostrar la media calculada
+    cout << "La media de los numeros ingresados es: " << media << endl;
     return 0;
 }
